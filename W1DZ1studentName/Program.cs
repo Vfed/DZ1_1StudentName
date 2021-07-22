86742e5fc0f22331d29690f908b7d9c3361906d1
Vfed <asser.282195@gmail.com>
2021-07-22 19:00:12 +0300

    Add project files.

---
+++ W1DZ1studentName/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace W1DZ1studentName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name:");
+            string name = Console.ReadLine();
+            name = name.Trim();
+            if (name.Length > 0)
+            {
+                Console.WriteLine("Hi ," + name +" you are a student now");
+            }
+            else
+            {
+                Console.WriteLine("It`s a problem you haven`t a name.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
